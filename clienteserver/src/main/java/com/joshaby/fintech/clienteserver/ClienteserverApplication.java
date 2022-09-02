2bb354bb853b09c8ec3813ea6934bd47b54b870a
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-09-01 22:31:56 -0300

    Adiciona projeto Cliente Server

---
+++ clienteserver/src/main/java/com/joshaby/fintech/clienteserver/ClienteserverApplication.java
@@ -0,0 +1,12 @@
+package com.joshaby.fintech.clienteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteserverApplication.class, args);
+	}
+}
